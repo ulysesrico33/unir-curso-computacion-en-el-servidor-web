5879b656fcd670dfa7a4a15d058516872ef6a424
Ulyses Rico Rea <ulyses.rico.2012@gmail.com>
2023-11-01 20:12:22 -0600

    ...

--- Programación-con-php/codigo1.php
+++ Programación-con-php/codigo1.php
@@ -1,10 +1,23 @@
 <?php
 
+# Conclusiones : Creo es Insertion Sort
+# No puede ser bubble sort porque el número de pasadas es siempre tamArreglo -1
+# No puede ser merge sort porque el algoritmo nunca parte el arreglo
+# No puede ser quick sort porque no hay pivote.
+
 function fx($x)
 {
+    $res=count($x)-1;
+    $pasada = 0;
+    echo "Número de elementos : $res \n";
     do { 
 
         $cont =false;
+        if ($cont == false)
+        {
+           $pasada++;
+
+        }
         $c=count($x)-1;
         for($i = 0; $i < $c ; $i++)
         { 
@@ -20,6 +33,7 @@ function fx($x)
         #cont = false si el arreglo ya está ordenado
     }while ($cont);
     
+    echo "Total de pasadas : $pasada \n";
     return $x;
 }
 
